b027b4b80551254e79cbb038dc0e419c0deef1b3
Rohit4174 <163422390+Rohit4174@users.noreply.github.com>
2025-10-28 23:27:55 +0530

    Add port parameter to app.run in app.py

--- backend/app.py
+++ backend/app.py
@@ -64,7 +64,7 @@ def receive_data():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0",port=port,debug=True)
 
 
 
